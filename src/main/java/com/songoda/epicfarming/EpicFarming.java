ba7b1eacf463366b2cfdb241a47f84f3fd60f250
Brianna <bri@songoda.com>
2020-07-24 11:05:27 -0500

    Don't allow the modifying of farm items in an anvil.

--- src/main/java/com/songoda/epicfarming/EpicFarming.java
+++ src/main/java/com/songoda/epicfarming/EpicFarming.java
@@ -24,10 +24,7 @@ import com.songoda.epicfarming.farming.levels.modules.Module;
 import com.songoda.epicfarming.farming.levels.modules.ModuleAutoBreeding;
 import com.songoda.epicfarming.farming.levels.modules.ModuleAutoButcher;
 import com.songoda.epicfarming.farming.levels.modules.ModuleAutoCollect;
-import com.songoda.epicfarming.listeners.BlockListeners;
-import com.songoda.epicfarming.listeners.EntityListeners;
-import com.songoda.epicfarming.listeners.InteractListeners;
-import com.songoda.epicfarming.listeners.UnloadListeners;
+import com.songoda.epicfarming.listeners.*;
 import com.songoda.epicfarming.settings.Settings;
 import com.songoda.epicfarming.storage.Storage;
 import com.songoda.epicfarming.storage.StorageRow;
@@ -192,6 +189,7 @@ public class EpicFarming extends SongodaPlugin {
         pluginManager.registerEvents(new BlockListeners(this), this);
         pluginManager.registerEvents(new InteractListeners(this), this);
         pluginManager.registerEvents(new UnloadListeners(this), this);
+        pluginManager.registerEvents(new InventoryListeners(), this);
 
         if (pluginManager.isPluginEnabled("FabledSkyBlock")) {
             try {
