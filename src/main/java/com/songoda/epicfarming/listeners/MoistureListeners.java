77c7b64f6612bde5d586295d83d0890472641d2e
Fernando Pettinelli <fernandopettinellifreire@gmail.com>
2022-07-03 16:24:32 -0400

    Version 3.2.2

---
+++ src/main/java/com/songoda/epicfarming/listeners/MoistureListeners.java
@@ -0,0 +1,28 @@
+package com.songoda.epicfarming.listeners;
+
+import com.songoda.epicfarming.EpicFarming;
+import com.songoda.epicfarming.farming.Farm;
+import org.bukkit.block.data.type.Farmland;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.MoistureChangeEvent;
+
+public class MoistureListeners implements Listener {
+
+    private final EpicFarming plugin;
+    public MoistureListeners(EpicFarming plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onMoistureChange(MoistureChangeEvent event) {
+        if (event.getNewState().getBlockData() instanceof Farmland) {
+            return;
+        }
+
+        Farm farm = plugin.getFarmManager().checkForFarm(event.getBlock().getLocation());
+        if (farm != null) {
+            event.setCancelled(true);
+        }
+    }
+}
