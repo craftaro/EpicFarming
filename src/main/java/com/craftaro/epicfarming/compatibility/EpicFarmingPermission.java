d19a6ff670561c918f4bf180734b64ce2c1fc9d7
TeamHR <hsingh20sep@gmail.com>
2024-03-26 23:52:53 +0530

    [Fixed] FabledSkyblock permission hook.

--- src/main/java/com/craftaro/epicfarming/compatibility/EpicFarmingPermission.java
+++ src/main/java/com/craftaro/epicfarming/compatibility/EpicFarmingPermission.java
@@ -1,6 +1,6 @@
 package com.craftaro.epicfarming.compatibility;
 
-import com.craftaro.skyblock.core.third_party.com.cryptomorin.xseries.XMaterial;
+import com.craftaro.third_party.com.cryptomorin.xseries.XMaterial;
 import com.craftaro.skyblock.permission.BasicPermission;
 import com.craftaro.skyblock.permission.PermissionType;
 
